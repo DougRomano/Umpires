a807d031005eab8cc2a7377b39d445e8213aea7a
DougRomano <doug.c.romano@gmail.com>
2011-04-21 13:45:27 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Umpires::Application.config.secret_token = '3c91c83e6daf805eac4e627bb264ed3f3422af465f28b3b09b345bfe9ddaff6619e7678a5336aaacd5b24711007ba43ad4ab6b28200659d4cb8f844967534d1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Umpires::Application.config.session_store :cookie_store, :key => '_Umpires_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Umpires::Application.config.session_store :active_record_store
